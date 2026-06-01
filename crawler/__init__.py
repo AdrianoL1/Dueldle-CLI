c262959ccf53e53841fea613a21cf15976ee798e
Holanddez <elfosupremo@hotmail.com>
2026-06-01 17:57:37 -0300

    feat: add crawler

---
+++ crawler/__init__.py
@@ -0,0 +1,2 @@
+from .crawler import get_champions_slugs
+from .crawler import get_champions_pages
---
+++ crawler/crawler.py
@@ -0,0 +1,48 @@
+import aiohttp
+from aiohttp import ClientSession
+import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATA_URL = os.getenv("DATA_URL")
+WIKI_URL = os.getenv("WIKI_URL")
+
+async def fetch_page(session: ClientSession, url: str) -> str:
+    async with session.get(url) as res:
+        return await res.text()
+
+async def get_champions_slugs() -> list[str]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(DATA_URL) as res:
+            data: dict = await res.json()
+                    
+    slugs = [f"{champion_data['name'].replace("'", '')}" for champion_data in data["data"].values()]
+
+    print(f"Total: {len(slugs)} champions")
+    return slugs
+
+async def get_champions_pages(slugs: list[str]) -> list[str]:
+    semaphore = asyncio.Semaphore(10)
+
+    async def fetch_champion(slug: str):
+        async with semaphore:
+            wiki_html, universe_html = await asyncio.gather(
+                fetch_page(session, f"{WIKI_URL}/{slug}"),
+                fetch_page(session, f"{WIKI_URL}/Universe:{slug}")
+            )
+            
+            return {
+                slug: {
+                    "wiki": wiki_html,
+                    "universe": universe_html
+                }
+            }
+            
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for slug in slugs:
+            tasks.append(fetch_champion(slug))
+        return await asyncio.gather(*tasks)
