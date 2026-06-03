4359dacb60a2e240a3260971ca236d2c46bb1628
Holanddez <elfosupremo@hotmail.com>
2026-06-03 00:33:54 -0300

    fix: correct field types in Character schema

--- models/champion.py
+++ models/champion.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Character(BaseModel):
-    name: str
+    name: str = Field(serialization_alias="championName")
     gender: str
     roles: list[str]
     species: list[str]
     regions: list[str]
-    resource = str
-    range = str
-    release_date = int
-    character_img_url = str
+    resource: str
+    range: str
+    release_date: int = Field(serialization_alias="releaseDate")
+    character_img_url: str = Field(serialization_alias="characterImgUrl")
+    
